58eec6270d53e1ae1c82bfbfff3ef2509bb61729
KangSangHyeok <ksh44820@naver.com>
2022-02-19 15:00:14 +0900

    practice tailwind css-2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,79 @@
 import type {NextPage} from 'next';
 
+// tailwind css => 모바일 우선 적용 
+
 const Home: NextPage = () => {
   return (
     // <div className="bg-slate-400 py-10 px-5 flex flex-col space-y-5">
-    <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-bold text-3xl">Select Item</span>
-        <div className="flex justify-between my-3">
-          <span className="font-semibold text-gray-500">Grey Chair</span>
-          <span>$19</span>
+    <div className="dark bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
+      <h1 className="text-8xl">Hello</h1>
+      <h1 className="text-[200px] text-[#d2d3d2] bg-[url('/vercel.svg')] bg-cover">Hello</h1>
+      {/* <details className="select-none open:text-white open:bg-indigo-400">
+        <summary className="cursor-pointer">What is my favorite food?</summary>
+        <span className="selection:bg-indigo-500 selection:text-white">meat</span>
+      </details> */}
+
+      {/* <input type="file" className="file:cursor-pointer file:hover:text-purple-400 file:hover:bg-white file:bover:border-purple-400 file:hover:border-2 file:transition-colors file:border-0 file:rounded-md file:px-5 file:text-white file:bg-purple-400"/>
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-400">Hello</p>
+
+      <form className="flex flex-col space-y-2 p-5">
+        <input 
+          type="text"
+          required
+          placeholder="Username"
+          className="border p-1 peer border-gray-400 rounded-xl"
+        />
+        <span className="hidden peer-invalid:block peer-invalid:text-red-500">This input is invalid</span>
+        <span className="hidden peer-valid:block peer-valid:text-teal-500">Awsome Username</span>
+
+        
+        <input type="submit" value="login" className="bg-white"/>
+      </form> */}
+
+      {/* <form className="flex flex-col space-y-2 bg-blue-200 p-5">
+        <input type="text" required placeholder="username" className="required:bg-yellow-500 invalid:bg-red-400 valid:bg-teal-200"/>
+        <input type="password" required placeholder="password"/>
+        <input type="submit" value="login" className="bg-white"/>
+      </form> */}
+      
+      <div className="bg-white dark:bg-black flex flex-col justify-between 
+      sm:bg-red-400 sm:hover:bg-red-600 md:bg-teal-400 lg:bg-indigo-400 xl:bg-purple-400 2xl:bg-pink-400 
+      p-6 rounded-3xl shadow-xl">
+        <span className="font-bold dark:text-white text-3xl">Select Item</span>
+        {/* <ul>
+          {[1,2,3,4].map((i) => 
+            <div className="flex justify-between my-2 odd:bg-blue-400 even:bg-yellow-200">
+              <span className="font-semibold text-gray-500">Grey Chair</span>
+              <span>$19</span>
+            </div>
+          )}
+        </ul>
+          
+        {["a", "b", "c", ""].map((c, i) => 
+          <li key={i} className="bg-red-500 py-2 empty:hidden list-none">{c}</li>)
+        } */}
+
+        <div>
+          <div className="flex justify-between my-2 ">
+            <span className="font-semibold text-gray-500 dark:text-gray-100 ">Grey Chair</span>
+            <span>$19</span>
+          </div>
+          <div className="flex justify-between my-2 ">
+            <span className="font-semibold text-gray-500 dark:text-gray-100 ">Grey Chair</span>
+            <span>$19</span>
+          </div>
         </div>
-        <div className="flex justify-between my-3">
-          <span className="font-semibold text-gray-500">Tooly Table</span>
-          <span>$80</span>
-        </div>
+
+
+
         <div className="mt-2 pt-2 border-t-2 border-dashed flex justify-between">
           <span className="font-semibold">Total</span>
           <span>$99</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 
+        <div className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white p-3 
           text-center rounded-xl w-1/2 mx-auto
+          cursor-pointer
+          dark:hover:bg-white
           hover:bg-teal-500 hover:text-black
           active:bg-yellow-500
           focus:bg-red-500
@@ -29,8 +83,8 @@ const Home: NextPage = () => {
         </div>
       </div>
       
-      <div className="bg-white overflow-hidden rounded-3xl shadow-2xl">
-        <div className="bg-blue-500 p-6 pb-14">
+      <div className="bg-white overflow-hidden rounded-3xl shadow-2xl group">
+        <div className="landscape:bg-yellow-400 portrait:bg-indigo-800 p-6 pb-14 xl:pb-44">
           <span className="text-white text-2xl">Profile</span>
         </div>
         <div className="rounded-3xl relative -top-5 bg-white p-6">
@@ -39,7 +93,8 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full"></div>
+            {/* avartar */}
+            <div className="h-24 w-24 bg-red-400 rounded-full group-hover:bg-red-300"/>
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,400</span>
@@ -52,8 +107,7 @@ const Home: NextPage = () => {
         </div>
       </div>
 
-      
-      <div className="bg-white p-6 rounded-3xl shadow-2xl">
+      <div className="bg-white p-6 rounded-3xl shadow-2xl lg:col-span-2 xl:col-span-1">
         {/* header */}
         <div className="flex mb-5 justify-between items-center">
           <span>👈</span>
@@ -92,6 +146,7 @@ const Home: NextPage = () => {
 
 
       </div>
+    
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
+  // darkMode: 'media', // media 다크모드 설정은 os 환경설정에 따라감.
+  darkMode: 'class', // 웹 사이트 커스텀 => 부모에 dark 클래스명 추가
   plugins: [],
-}
+};
