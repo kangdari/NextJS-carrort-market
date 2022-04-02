55776efe1f95d3a688d797c4ee4caa48344f8d25
KangSangHyeok <ksh44820@naver.com>
2022-04-02 14:43:14 +0900

    twilio, sendgrid 환경 변수 추가 / token - 문자 전송, 이메일 전송 api 추가

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,13 +1,19 @@
+import twilio from 'twilio';
+import mail from '@sendgrid/mail';
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
+
+mail.setApiKey(process.env.SEND_GRID_API_KEY!);
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : email ? { email } : null;
+  const user = phone ? { phone } : email ? { email } : null;
   if (!user) return res.status(400).json({ ok: false });
 
   const payload = Math.floor(10000 + Math.random() * 999999) + '';
@@ -29,6 +35,22 @@ async function handler(
       },
     },
   });
+
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.MESSAGING_SERVICE_ID,
+      to: process.env.PHONE_NUM!,
+      body: `your login token is ${payload}`,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: 'ksh44820@naver.com',
+      to: 'ksh44820@naver.com',
+      subject: 'your carrot market Verification Email',
+      text: `your token is ${payload}`,
+      html: `<strong>your token is ${payload}</strong>`,
+    });
+  }
 
   return res.json({
     ok: true,
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -27,10 +27,6 @@ export default function Enter() {
     enter(validForm);
   };
 
-  console.log(`loading: ${loading}`);
-  console.log(data);
-  console.log(error);
-
   return (
     <div className='mt-16 px-4'>
       <h3 className='text-3xl font-bold text-center'>Enter to Carrot</h3>
