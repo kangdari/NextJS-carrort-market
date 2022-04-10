273dbec5758be80c0116384f2590b9fec44562b7
KangSangHyeok <ksh44820@naver.com>
2022-04-10 15:27:46 +0900

    withHandler isPrivate(로그인 필요 여부)  옵션 추가

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -5,23 +5,32 @@ export interface ResponseType {
   [key: string]: any;
 }
 
+interface ConfigType {
+  method: 'GET' | 'POST' | 'DELETE';
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean; // 로그인 상태
+}
+
 // HOF
 // 하나 이상의 함수를 인자로 받고
 // 함수를 반환하는 함수
-export default function withHandler(
-  method: 'GET' | 'POST' | 'DELETE',
-  fn: (req: NextApiRequest, res: NextApiResponse) => void
-) {
+export default function withHandler(config: ConfigType) {
+  const { method, handler, isPrivate = true } = config;
+
   return async function (
     req: NextApiRequest,
     res: NextApiResponse
   ): Promise<any> {
     if (req.method !== method) {
-      // bad request
       return res.status(405).end();
     }
+
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, error: 'please login...' });
+    }
+
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ error });
